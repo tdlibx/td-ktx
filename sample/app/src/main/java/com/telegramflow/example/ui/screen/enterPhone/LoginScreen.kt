fc56e4cd7e88ee2973eb6bfc897a7d24c0e1b7b1
akrafts-gpt <akrafts.gpt@gmail.com>
2025-11-22 20:32:18 +0100

    Fix compose state delegate imports

--- sample/app/src/main/java/com/telegramflow/example/ui/screen/enterPhone/LoginScreen.kt
+++ sample/app/src/main/java/com/telegramflow/example/ui/screen/enterPhone/LoginScreen.kt
@@ -28,8 +28,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
