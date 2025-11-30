e1ca2cd0a1b834bacb5f861d2704db6b08ccbb78
Alexey Kravtsov <alexey.kravtsov@truecaller.com>
2025-11-30 21:28:41 +0000

    update lib versions

--- sample/app/build.gradle.kts
+++ sample/app/build.gradle.kts
@@ -67,9 +67,6 @@ android {
         compose = true
         buildConfig = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
